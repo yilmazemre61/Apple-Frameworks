760ee78ea69b2b8747a91ac1830260b5e276dd80
yilmazemre61 <emreyilmz1@gmail.com>
2024-02-23 01:34:41 -0500

    FrameworkGridView changed

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  FrameworkGridView.swift
 //  Apple-Frameworks
 //
 //  Created by Emre Yilmaz on 2024-02-23.
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ScrollView{
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks){ framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
         }
-        .padding()
     }
 }
 
 #Preview {
     FrameworkGridView()
+}
+
+
+struct FrameworkTitleView: View {
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            
+            Text(framework.name)
+                .font(.title)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
 }
